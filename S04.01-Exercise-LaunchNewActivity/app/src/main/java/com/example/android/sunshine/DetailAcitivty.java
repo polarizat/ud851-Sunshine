77cdb706ae1c246ff317ac86991cc43216fd4aa4
Sergiu Apostul <sergiu.apostul@gmail.com>
2020-05-23 18:52:57 +0300

    Migrate to AndroidX and Solved all TODOs for S04.01

---
+++ S04.01-Exercise-LaunchNewActivity/app/src/main/java/com/example/android/sunshine/DetailAcitivty.java
@@ -0,0 +1,14 @@
+package com.example.android.sunshine;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class DetailAcitivty extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_detail_acitivty);
+    }
+}
