3858aa62da435b6a4e6cae7597e56a7d95f9ae9d
Sergiu Apostul <sergiu.apostul@gmail.com>
2020-05-23 23:11:28 +0300

    Migrate to AndroidX and update the dependencies. Solved all TODOs for S04.02

--- S04.02-Exercise-DisplayDayForecast/app/src/main/java/com/example/android/sunshine/DetailActivity.java
+++ S04.02-Exercise-DisplayDayForecast/app/src/main/java/com/example/android/sunshine/DetailActivity.java
@@ -1,7 +1,10 @@
 package com.example.android.sunshine;
 
+import android.content.Intent;
 import android.os.Bundle;
-import android.support.v7.app.AppCompatActivity;
+import android.widget.Toast;
+
+import androidx.appcompat.app.AppCompatActivity;
 
 public class DetailActivity extends AppCompatActivity {
 
@@ -13,5 +16,11 @@ public class DetailActivity extends AppCompatActivity {
         setContentView(R.layout.activity_detail);
 
         // TODO (2) Display the weather forecast that was passed from MainActivity
+        Intent intent = getIntent();
+        if (intent.getStringExtra(Intent.EXTRA_TEXT) != null) {
+            String weather = getIntent().getStringExtra(Intent.EXTRA_TEXT);
+            Toast.makeText(this, weather, Toast.LENGTH_SHORT).show();
+
+        }
     }
 }
